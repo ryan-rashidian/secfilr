72ddc5a8457295709e639700c81578de7bdde31f
ryan-rashidian <ryanrashidian@gmail.com>
2026-02-10 11:32:03 -0800

    Update test script

--- tests/test_secfilr.py
+++ tests/test_secfilr.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from secfilr.fetch import FetchBulk
 from secfilr.company import Company, Metric
+from secfilr.company import StatementType as stmnt
 
 BASE_PATH = Path('/path/to/bulkdata')
 
@@ -23,9 +24,25 @@ def main():
     print(aapl.ticker)
     print(aapl.name)
     print(assets.unit)
-    print(assets.label)
-    print(assets.description)
-    print(assets.filings[0])
+    print(f'{assets.label}: {assets.description}')
+    print('\nFILING DATA:')
+    recent_filing: dict = assets.filings[-1]
+    for key, value in recent_filing.items():
+        print('----------')
+        print(key)
+        print(value)
+    print('----------')
+
+    balance_sheet: dict = aapl.statement(
+        statement_t = stmnt.BALANCE_SHEET,
+        quarter_off = 0 # Quarter offset `0`; most recent statement
+    )
+    print('\nBALANCE SHEET:')
+    for metric, value in balance_sheet.items():
+        print('----------')
+        print(metric)
+        print(value)
+    print('----------')
 
 
 if __name__ == '__main__':
