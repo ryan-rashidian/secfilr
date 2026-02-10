5716270a512c1a33d28f4f1c960cc0c267ba20b3
ryan-rashidian <ryanrashidian@gmail.com>
2026-02-10 11:15:19 -0800

    Implement statements, move argument -> mapping label logic into Company before Parser is called

--- src/secfilr/_parse.py
+++ src/secfilr/_parse.py
@@ -3,25 +3,16 @@
 Parse concepts into categorized metrics defined by secfilr.
 """
 
-from secfilr import _xbrl_labels
-from secfilr.exceptions import ParsingError, InvalidMetric
+from secfilr.exceptions import ParsingError
 from secfilr._models import Metric
 
 
 class ParseMetric:
     """Metric parser."""
 
-    def __init__(self, raw_facts: dict):
+    def __init__(self, raw_facts: dict) -> None:
         """Initialize raw companyfacts data."""
         self.raw_facts = raw_facts
-
-    def _get_mappings(self, metric: str) -> tuple[str]:
-        """Get matching tuple for concept mapping."""
-        try:
-            section, label = _xbrl_labels.map_arg[metric]
-            return _xbrl_labels.statements[section][label]
-        except KeyError as e:
-            raise InvalidMetric(f'{metric} is undefined') from e
 
     def _map_to_metric(self, xbrl_mappings: tuple[str]) -> dict:
         """Map XBRL labels to categorized metric."""
@@ -32,14 +23,13 @@ class ParseMetric:
         raise ParsingError('Mapping: No matches found.')
 
     def _get_units(self, units_dict: dict | None) -> str:
-        """Get unit key value for concept."""
+        """Get unit key for concept."""
         if not units_dict:
             raise ParsingError('Filing data not found.')
         return next(iter(units_dict))
 
-    def parse(self, metric: str) -> Metric:
+    def parse(self, xbrl_mapping: tuple[str]) -> Metric:
         """Get parsed Concept from an xbrl_mapping."""
-        xbrl_mapping = self._get_mappings(metric.lower())
         concept: dict = self._map_to_metric(xbrl_mapping)
         label: str = concept.get('label', '')
         description: str = concept.get('description', '')
--- src/secfilr/company.py
+++ src/secfilr/company.py
@@ -4,9 +4,20 @@
 and any processing and parsing that secfilr may do.
 """
 
+from enum import StrEnum
+
+from secfilr import _xbrl_labels
+from secfilr.exceptions import ParsingError, InvalidMetric
 from secfilr.fetch import Fetch
 from secfilr._models import CompanyFacts, Facts, Metric
 from secfilr._parse import ParseMetric as _ParseMetric
+
+
+class StatementType(StrEnum):
+    """Enumerations for statment keys."""
+    BALANCE_SHEET = 'Balance Sheet'
+    CASH_FLOW_STATEMENT = 'Cash Flow Statement'
+    INCOME_STATEMENT = 'Income Statement'
 
 
 class Company:
@@ -17,7 +28,7 @@ class Company:
         fetcher (Fetch): data fetcher
     """
 
-    def __init__(self, ticker: str, fetcher: Fetch):
+    def __init__(self, ticker: str, fetcher: Fetch) -> None:
         """Initialize companyfacts and parser using given fetcher."""
         companyfacts: CompanyFacts = fetcher.companyfacts(ticker)
         self.facts: Facts = companyfacts.facts
@@ -44,6 +55,14 @@ class Company:
         else:
             return None
 
+    def _get_metric_mapper(self, metric: str) -> tuple[str]:
+        """Get matching tuple for metric mapping."""
+        try:
+            section, label = _xbrl_labels.map_arg[metric]
+            return _xbrl_labels.statements[section][label]
+        except KeyError as e:
+            raise InvalidMetric(f'{metric} is undefined') from e
+
     def metric(self, metric: str) -> Metric:
         """Get a parsed metric.
 
@@ -55,12 +74,31 @@ class Company:
             ParseError: if parsing fails
         """
         parser = _ParseMetric(self.facts.concepts)
-        return parser.parse(metric)
+        xbrl_mapping = self._get_metric_mapper(metric.lower())
+        return parser.parse(xbrl_mapping)
 
-    def statement(self, statement: str):
-        """Get parsed statements.
+    def statement(
+        self,
+        statement_t: StatementType,
+        quarter_off: int = 1
+    ) -> dict:
+        """Build a statement from parsed metrics."""
+        parser = _ParseMetric(self.facts.concepts)
+        statement_dict = {}
+        filed, form = None, None
 
-        PLACEHOLDER
-        """
-        pass
+        for concept, labels in _xbrl_labels.statements[statement_t].items():
+            try:
+                parsed_metric: Metric = parser.parse(labels)
+                latest_filing: dict = parsed_metric.filings[-quarter_off]
+                value = latest_filing.get('val')
+                form = latest_filing.get('form')
+                filed = latest_filing.get('filed')
+                statement_dict[concept] = value
+            except ParsingError:
+                statement_dict[concept] = None
+        statement_dict['filed'] = filed
+        statement_dict['form'] = form
+
+        return statement_dict
 
